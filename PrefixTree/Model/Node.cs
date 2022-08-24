5d6bcf1725b1fc42bab3a838b1486f945210d986
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-08-24 18:42:54 +0300

    Добавлены комментарии и проверка входных параметров.

--- PrefixTree/Model/Node.cs
+++ PrefixTree/Model/Node.cs
@@ -6,30 +6,75 @@ using System.Threading.Tasks;
 
 namespace PrefixTree.Model
 {
+    /// <summary>
+    /// Узел.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     class Node<T>
     {
+        /// <summary>
+        /// Данные
+        /// </summary>
         public T Data { get; set; }
 
+        /// <summary>
+        /// Символ.
+        /// </summary>
         public char Symbol { get; set; }
 
+        /// <summary>
+        /// Флаг конечного узла в слове.
+        /// </summary>
         public bool IsWord = false;
 
+        /// <summary>
+        /// Список дочерних узлов.
+        /// </summary>
         public Dictionary<char, Node<T>> Items { get; set; }
 
+        /// <summary>
+        /// Создать новый узел.
+        /// </summary>
         public Node()
         {
             Symbol = '\0';
             Items = new Dictionary<char, Node<T>>();
         }
+
+        /// <summary>
+        /// Создать новый узел.
+        /// </summary>
+        /// <param name="data">Данные.</param>
+        /// <param name="symbol">Символ.</param>
         public Node(T data, char symbol)
         {
-            Data = data;
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data), "Данные не могут быть null");
+            }
+
+            if (symbol == '\0')
+            {
+                throw new ArgumentNullException(nameof(symbol), "Символ не может быть пустым");
+            }
+
+            Data = data; ;
             Symbol = symbol;
             Items = new Dictionary<char, Node<T>>();
         }
 
+        /// <summary>
+        /// Поиск дочернего узла с указанным символом.
+        /// </summary>
+        /// <param name="symbol">Символ.</param>
+        /// <returns>Дочерний узел с указанным символом.</returns>
         public Node<T> TryFind(char symbol)
         {
+            if (symbol == '\0')
+            {
+                throw new ArgumentNullException(nameof(symbol), "Символ не может быть пустым");
+            }
+
             if (Items.TryGetValue(symbol, out Node<T> value))
             {
                 return value;
--- PrefixTree/Model/Tree.cs
+++ PrefixTree/Model/Tree.cs
@@ -6,24 +6,63 @@ using System.Threading.Tasks;
 
 namespace PrefixTree.Model
 {
+    /// <summary>
+    /// Префиксное дерево.
+    /// </summary>
+    /// <typeparam name="T">Тип данных хранящихся в элементах дерева.</typeparam>
     class Tree<T>
     {
-        private string Key;
-
+        /// <summary>
+        /// Корневой элемент.
+        /// </summary>
         private Node<T> Head { get; set; }
 
+        /// <summary>
+        /// Создать новое дерево.
+        /// </summary>
         public Tree()
         {
             Head = new Node<T>();
         }
 
+        /// <summary>
+        /// Добавить новое слово.
+        /// </summary>
+        /// <param name="word">Слово.</param>
+        /// <param name="data">Данные.</param>
         public void Add(string word, T data)
         {
+            if (string.IsNullOrWhiteSpace(word))
+            {
+                throw new ArgumentNullException("Слово не может быть пустым или null", nameof(word));
+            }
+
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data), "Данные не могут быть null");
+            }
+
             Add(word, data, Head);
         }
 
+        /// <summary>
+        /// Добавить новое слово.
+        /// </summary>
+        /// <param name="word">Слово.</param>
+        /// <param name="data">Данные.</param>
+        /// <param name="node">Корневой узел.</param>
         private void Add(string word, T data, Node<T> node)
         {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data), "Данные не могут быть null");
+            }
+
+            if (node is null)
+            {
+                throw new ArgumentNullException(nameof(node), "Узел не может быть null");
+            }
+
             if (string.IsNullOrEmpty(word))
             {
                 if (!node.IsWord)
@@ -50,13 +89,32 @@ namespace PrefixTree.Model
 
         }
 
+        /// <summary>
+        /// Удалить слово.
+        /// </summary>
+        /// <param name="word">Слово.</param>
         public void Remove(string word)
         {
+            if (string.IsNullOrWhiteSpace(word))
+            {
+                throw new ArgumentException("Слово не может быть пустым или null", nameof(word));
+            }
+
             Remove(word, Head);
         }
 
+        /// <summary>
+        /// Удалить слово.
+        /// </summary>
+        /// <param name="word">Слово</param>
+        /// <param name="node">Корневой узел.</param>
         private void Remove(string word, Node<T> node)
         {
+            if (node is null)
+            {
+                throw new ArgumentNullException(nameof(node), "Узел не может быть null");
+            }
+
             if (string.IsNullOrEmpty(word))
             {
                 if (node.IsWord)
@@ -73,16 +131,37 @@ namespace PrefixTree.Model
                 }
 
             }
-
         }
 
+        /// <summary>
+        /// Проверка есть ли такое слово.
+        /// </summary>
+        /// <param name="word">Искомое слово.</param>
+        /// <returns>Результат проверки.</returns>
         public bool TrySearch(string word)
         {
+            if (string.IsNullOrWhiteSpace(word))
+            {
+                throw new ArgumentException("Слово не может быть пустым или null", nameof(word));
+            }
+
             return TrySearch(word, Head, out T value);
         }
 
+        /// <summary>
+        /// Проверка есть ли такое слово.
+        /// </summary>
+        /// <param name="word">Искомое слово.</param>
+        /// <param name="node">Корневой узел.</param>
+        /// <param name="value">Данные искомого слова.</param>
+        /// <returns>Результат проверки.</returns>
         private bool TrySearch(string word, Node<T> node, out T value)
         {
+            if (node is null)
+            {
+                throw new ArgumentNullException(nameof(node), "Узел не может быть null");
+            }
+
             value = default(T);
             var result = false;
             if (string.IsNullOrEmpty(word))
